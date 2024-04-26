dc4acbc3b79491c3ba45c14fa3f37ec8bedb188f
Pedro <pedroarmiliato2006@gmail.com>
2024-04-26 16:44:27 -0300

    C:\Users\armiliato.8980\Desktop\GitHub\Senac-atividade-\faq-accordion-main

---
+++ faq-accordion-main/script.js
@@ -0,0 +1,8 @@
+function toggleAccordion(header) {
+    const content = header.nextElementSibling;
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+    } else {
+        content.style.display = 'block';
+    }
+}
